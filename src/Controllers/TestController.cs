68dd3893551b78804c24e505ebab781d881bfac0
Slawomir Brys <Slawomir.Brys@sonova.com>
2022-09-02 16:07:11 +0200

    Runtime version check

---
+++ src/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices;
+
+namespace dotnet_runtime_version.Controllers
+{
+    public class TestController : Controller
+    {
+        [HttpGet("/version")]
+        public async Task<ActionResult> GetVersion()
+        {
+            var result = new
+            {
+                RuntimeIdentifier = RuntimeInformation.RuntimeIdentifier,
+                FrameworkDescription = RuntimeInformation.FrameworkDescription,
+                OSArchitecture = RuntimeInformation.OSArchitecture.ToString("F"),
+                OSDescription = RuntimeInformation.OSDescription,
+                ProcessArchitecture = RuntimeInformation.ProcessArchitecture.ToString("F"),
+                IsFreeBsd = RuntimeInformation.IsOSPlatform(OSPlatform.FreeBSD),
+                IsLinux = RuntimeInformation.IsOSPlatform(OSPlatform.Linux),
+                IsOsx = RuntimeInformation.IsOSPlatform(OSPlatform.OSX),
+                IsWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows),
+            };
+
+            return Ok(result);
+        }
+    }
+}
